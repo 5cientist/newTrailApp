641fa399e98d67a14ff6c28b02c9c339e26cbee1
hanshir <hanshirlukku@gmail.com>
2020-09-01 00:36:03 +0530

    added mainaxisaliginment to row

--- lib/screens/homepage/homepage.dart
+++ lib/screens/homepage/homepage.dart
@@ -4,8 +4,26 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Text("this is a Home Page"),
+      body: SafeArea(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              padding: EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black12),
+                  borderRadius: BorderRadius.circular(5.0)),
+              child: Icon(Icons.menu),
+            ),
+            Container(
+              padding: EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black12),
+                  borderRadius: BorderRadius.circular(5.0)),
+              child: Icon(Icons.menu),
+            ),
+          ],
+        ),
       ),
     );
   }
