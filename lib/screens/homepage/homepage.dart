ae9df9e33eabf3a51b0858bfadd63db71c89ecad
hanshir <hanshirlukku@gmail.com>
2020-09-01 08:14:33 +0530

    added padding to singlechildscrollview

--- lib/screens/homepage/homepage.dart
+++ lib/screens/homepage/homepage.dart
@@ -6,7 +6,23 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: CustomAppBar(),
+        child: SingleChildScrollView(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CustomAppBar(),
+              Text(
+                "City",
+                style: TextStyle(
+                  color: Colors.black54,
+                  fontSize: 17,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
