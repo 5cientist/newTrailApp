df5692527ff9dc5900169c21c5d9e04d58b482bc
hanshir <hanshirlukku@gmail.com>
2020-09-01 08:52:20 +0530

    added GreyBx

--- lib/screens/homepage/homepage.dart
+++ lib/screens/homepage/homepage.dart
@@ -20,8 +20,64 @@ class HomePage extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                 ),
               ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10.0),
+                    child: Text(
+                      "San Francisco",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 34,
+                      ),
+                    ),
+                  ),
+                  Icon(Icons.more_vert),
+                ],
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: 60.0,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    GreyBx(title: "For Sale"),
+                    GreyBx(title: "<\$220.00>"),
+                    GreyBx(title: "3 - 4 beds"),
+                    GreyBx(title: "3 - 4 beds"),
+                  ],
+                ),
+              ),
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class GreyBx extends StatelessWidget {
+  const GreyBx({
+    Key key,
+    this.title,
+  }) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      padding: EdgeInsets.all(10.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30.0),
+        color: Colors.black54,
+      ),
+      child: Text(
+        title,
+        style: TextStyle(
+          fontSize: 17,
         ),
       ),
     );
